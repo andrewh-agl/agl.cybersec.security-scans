1cf36ac6285aa2995d6ff7e8ab9453232bf2898c
Sajid Pervez <A30003153@agl.com.au>
2019-10-25 11:34:16 +1100

    changes

--- ci-security.sh
+++ ci-security.sh
@@ -19,13 +19,12 @@ dotnet() {
     # Register Microsoft key and feed
     wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
-    sudo dpkg -i packages-microsoft-prod.deb
     # Install .NET SDK
     sudo add-apt-repository universe
     sudo apt-get update
-    sudo apt-get install apt-transport-https
+    sudo apt-get install --ignore-missing apt-transport-https
     sudo apt-get update
-    sudo apt-get install dotnet-sdk-3.0
+    sudo apt-get install --ignore-missing dotnet-sdk-3.0
     # if [ $? -eq 0 ]; then
     #     sudo -i dpkg --purge packages-microsoft-prod && sudo dpkg -i packages-microsoft-prod.deb
     #     sudo apt-get update
