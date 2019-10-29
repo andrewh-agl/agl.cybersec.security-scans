0ff5b2df3ac7d5c8c056a35d00e20472ca391313
Sajid Pervez <A30003153@agl.com.au>
2019-10-29 13:10:15 +1100

    changes

--- ci-security.sh
+++ ci-security.sh
@@ -128,14 +128,14 @@ echo $b64bom
 #         -F "project=${PROJECT_UUID}" \
 #         -F "bom=${b64bom}"
 
-TOKEN = curl -i -X "PUT" "http://104.43.15.124:443/api/v1/bom" \
+REQ="$(curl -i -X "PUT" "http://104.43.15.124:443/api/v1/bom" \
             -H 'Content-Type: application/json' \
             -H "X-API-Key: ${API_KEY}" \
             -d '{
                 "project": "'${PROJECT_UUID}'",
                 "bom": "'${b64bom}'"
-            }'
-echo $TOKEN
+            }')"
+echo $REQ | grep body
 exit 0
 # Pool DT and pull results when ready
 while [ $TOKEN -ne false ]
