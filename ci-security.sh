22714756475ad3829ea4c6d68604f6ef09151d93
Sajid Pervez <A30003153@agl.com.au>
2019-10-31 15:57:20 +1100

    npm changes

--- ci-security.sh
+++ ci-security.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 # Exit on error
-#set -e
+set -e
 
 # Functions
 python_tool_install() {
@@ -110,7 +110,7 @@ b64bom=$(base64 -w 0 $DIR/bom.xml)
 mv $DIR/bom.xml .
 #echo $b64bom
 #5. Post sbom to depenedency track
-RES=$(curl -i -X "POST" "http://104.43.15.124:443/api/v1/bom" \
+RES=$(curl -i -X "PUT" "http://104.43.15.124:443/api/v1/bom" \
         -H "Content-Type:text/xml;charset=UTF-8" \
         -H "X-API-Key: ${API_KEY}" \
         -d @bom.xml)
