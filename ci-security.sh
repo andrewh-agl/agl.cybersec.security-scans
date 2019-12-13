8e3786d5c17e5b59190fd1607c93df1619f27b7a
Sajid Pervez <A30003153@agl.com.au>
2019-12-13 16:47:53 +1100

    changes

--- ci-security.sh
+++ ci-security.sh
@@ -32,10 +32,9 @@ dotnet_tool_install() {
 }
 
 node_install() {
-    sudo rm -rf /var/lib/apt/lists/*
-    sudo apt-get clean
     sudo apt-get install curl
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get update
     sudo apt-get install nodejs && node -v && npm -v
 }
 
