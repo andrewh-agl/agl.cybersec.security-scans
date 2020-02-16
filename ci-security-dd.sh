900d6d81aec147b4aa5a92a5f0bf67cc3d690efd
Sajid Pervez <A30003153@agl.com.au>
2020-02-17 10:52:29 +1100

    changes

--- ci-security-dd.sh
+++ ci-security-dd.sh
@@ -29,7 +29,7 @@ dotnet_tool_install() {
     sudo apt-get update 
     sudo apt-get install -y apt-transport-https
     sudo apt-get update
-    sudo apt-get install dotnet-sdk-2.1
+    sudo apt-get install -y dotnet-sdk-2.1
 }
 
 node_install() {
