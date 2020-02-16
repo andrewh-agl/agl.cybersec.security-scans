b17d334d5ec4223d649a29950019ff549ad33159
Sajid Pervez <A30003153@agl.com.au>
2020-02-17 10:42:10 +1100

    changes

--- ci-security-dd.sh
+++ ci-security-dd.sh
@@ -25,10 +25,11 @@ dotnet_tool_install() {
     wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
     # Install .NET SDK
-    sudo apt-get update
     sudo add-apt-repository universe
+    sudo apt-get update 
     sudo apt-get install -y apt-transport-https
-    sudo apt-get install dotnet-sdk-3.0
+    sudo apt-get update
+    sudo apt-get install dotnet-sdk-2.1
 }
 
 node_install() {
