2132b57a518de051fd390a52a54a526726fe8f67
Sajid Pervez <A30003153@agl.com.au>
2020-02-18 16:56:19 +1100

    changes

--- ci-security-dd.sh
+++ ci-security-dd.sh
@@ -441,7 +441,6 @@ dd_upload(){
         # Re-import scan to test ID
         echo $ENG_ID
         echo $TEST_ID
-        ls -ltr
         # Re-import scan
         RES="$(curl -k --silent -X -H "Authorization: ${DD_API_KEY}" \
         -F "scan_date=${d}" \
